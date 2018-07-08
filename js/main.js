e10ab030451dfb53a49f6ab1b012099c5fe3a22d
OaklandHawk <hawks93187@gmail.com>
2018-07-08 12:22:32 -0700

    new commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-main.js
+var myH1 = document.getElementById("myH1");
+	myH1.innerText = "Dogs are awesome!";
+var myNewH1 = document.getElementById("myNewH1");
+
+function getInfo() {
+	var userInput = document.getElementById("userInput").value;
+	console.log(userInput);
+		myNewH1.innerText = userInput;
+}
+
+
+
+var stringArray = ["Hi", "my", "name", "is", "Taylor"];
+
+for(var i = 0; i < stringArray.length; i++) {
+	console.log(stringArray[i]);
+} 
+
+
+
+var myObj = {
+	name: "Taylor",
+	age: "24",
+	isMale: true
+};
+
+console.log(myObj.name);
+console.log(myObj.age);
+console.log(myObj.isMale);
+
+var myH2 = document.getElementById("myH2");
+var myH3 = document.getElementById("myH3");
+var myH4 = document.getElementById("myH4");
+
+	myH2.innerText = myObj.name;
+	myH3.innerText = myObj.age;
+	myH4.innerText = myObj.isMale;
+
+
+
